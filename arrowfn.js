cbe23b1e4ab0719fef61fedfd1e68734ee7374f0
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-12 01:07:58 +0545

    Some more work

--- arrowfn.js
+++ arrowfn.js
@@ -6,3 +6,16 @@ console.log(sum(a,b))
 
 const cube = a => a*a*a;
 console.log(cube(a));
+
+function letssee(a,b){
+    return a+b;
+}
+console.log(letssee(1,1))
+
+
+const lower = (string) => string.toLowerCase();
+console.log(lower("HELLO SANTOSH"));
+
+
+const shorterThen = (array1, array2)=> array1 < array2;
+console.log(shorterThen([1,2,3], [1,3,4,4]))
--- typeof.js
+++ typeof.js
@@ -1,3 +1,33 @@
 const constant = "hello santosh";
-constant = "hello santosh";
+// constant = "hello santosh";
 console.log(constant)
+
+console.log(typeof "true");
+console.log(typeof  true );
+console.log("true"==true)
+
+console.log("----------------------------------")
+const variable = "santosh";
+const logger = () => {
+    console.log(variable)
+}
+logger();
+
+
+console.log("----------------------------------")
+
+const global = "hello there";
+function call() {
+    const global = "hi there";
+    console.log(global);
+}
+call();
+
+console.log("----------------------------------")
+
+function call() {
+     another_global = "hi there";
+    console.log(another_global);
+}
+call();
+console.log(another_global);
