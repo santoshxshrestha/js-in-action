d9e9b06bf8f64ba274e39931dc9ff23624097f0f
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-11 10:04:45 +0545

    fn

---
+++ fn.js
@@ -0,0 +1,8 @@
+function add(a,b) {
+    return a+b;
+}
+
+let a = 2;
+let b = 2;
+let sum = add(a,b);
+console.log(`The sum is =`,sum)
--- typeof.js
+++ typeof.js
@@ -1,4 +1,3 @@
-let a  = "hello"[3]
-console.log(a)
-let b = "waht is hello"
-console.log(b.indexOf("llo"))
+const constant = "hello santosh";
+constant = "hello santosh";
+console.log(constant)
