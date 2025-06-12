9fdf7fa6ff87439e21bffefe615e13f5e5e7c21b
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-12 21:53:54 +0545

    Add: nothing

--- setTimeout.js
+++ setTimeout.js
@@ -1,17 +1,18 @@
-let string = "hello there"
-
-let fn = function(variable) {
-    setTimeout(() => {
-        console.log(variable)
-    }, 1000);
-}
-fn(string)
-console.log("lol")
-
+// let string = "hello there"
+//
+// let fn = function(variable) {
+//     setTimeout(() => {
+//         console.log(variable)
+//     }, 1000);
+// }
+// fn(string)
+// console.log("lol")
+//
 
 console.log("------------------------------")
 console.log("hello there again ")
 setTimeout(() => {
-    
-}, 2000);
+    console.log("this is inside the settimeout block")
+}, 2000).await;
+console.log("this is at the end of the settimeout block")
 
