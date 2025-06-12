e82731b850f89487a35a8a3983879725cdd99477
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-12 21:48:12 +0545

    Add: content

--- setTimeout.js
+++ setTimeout.js
@@ -3,7 +3,15 @@ let string = "hello there"
 let fn = function(variable) {
     setTimeout(() => {
         console.log(variable)
-    }, 1000).await;
+    }, 1000);
 }
-
 fn(string)
+console.log("lol")
+
+
+console.log("------------------------------")
+console.log("hello there again ")
+setTimeout(() => {
+    
+}, 2000);
+
