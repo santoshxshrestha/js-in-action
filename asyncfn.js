810363ae40497e267f92af90297e6eab9632a3cd
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-13 13:23:04 +0545

    Add: content

--- asyncfn.js
+++ asyncfn.js
@@ -1,8 +1,7 @@
 async function fetchResponse(url){
     const response = await fetch(url);
-    // let {message} = await response.json();
-    // return await message;
-    return response
+    let {message} = await response.json();
+    return await message;
 }
 async function main() {
     let url = "https://dog.ceo/api/breed/hound/list";
