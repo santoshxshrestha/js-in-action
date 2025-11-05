03cd9f05f766e685d7eea87ad983c145623491ee
santoshxshrestha <santoshxshrestha@gmail.com>
2025-11-05 22:56:11 +0545

    this will do

--- asm/add.asm
+++ asm/add.asm
@@ -1,9 +1,13 @@
-;sub two num
-;let 1000h = 01
-;let 1001h  = 01
-;result will be in 1002h 
-LHLD    1000h
-LDA     1001h
-ADD     H
-STA     1002h
-HLT
+; lets assume that we have the content in the memory in the 1000h and 1001h address
+lda 1000h ; here loading the first number from the memory to the accumulator
+add 1001h ; here adding the second number from the memory to the accumulator
+sta 1002h ; here storing the result back to the memory at address 1002h
+
+jnc no_carry ; jump if no carry to no_carry label
+mvi a, 01h; ; move immediate 01h to accumulator
+sta 1003h ; store the carry flag (1) to memory address 1003
+hlt        ; halt the program
+
+no_carry: mvi a, 00h; ; move immediate 00h to accumulator
+sta 1003h ; store the carry flag (0) to memory address 1003h
+hlt        ; halt the program
